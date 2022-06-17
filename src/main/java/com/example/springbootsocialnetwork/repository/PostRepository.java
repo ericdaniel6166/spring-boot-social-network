c8490880ee10b05536626536b5f0721984185422
eric6166 <ericnguyen6166@gmail.com>
2022-06-17 23:02:01 +0700

    add repository

--- src/main/java/com/example/springbootsocialnetwork/repository/PostRepository.java
+++ src/main/java/com/example/springbootsocialnetwork/repository/PostRepository.java
@@ -1,16 +1,9 @@
 package com.example.springbootsocialnetwork.repository;
 
-import com.example.springbootsocialnetwork.entity.Forum;
 import com.example.springbootsocialnetwork.entity.Post;
-import com.example.springbootsocialnetwork.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findAllByForum(Forum forum);
-
-    List<Post> findByUser(User user);
 }
